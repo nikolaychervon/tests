61f1a82e713ca15875037eac40010bea43b76dc5
Nikolay Chervon <nikolaychervon25@gmail.com>
2025-12-23 06:17:07 +0500

    Added view for users list.

--- test2/src/resources/views/app.blade.php
+++ test2/src/resources/views/app.blade.php
@@ -1,22 +1,32 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}"  @class(['dark' => ($appearance ?? 'system') == 'dark'])>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Список пользователей</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container mt-5">
+    <h2>Список пользователей</h2>
+    <div id="user-list" class="list-group mt-3"></div>
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <script>
+        fetch('/api/list')
+            .then(res => res.json())
+            .then(users => {
+                const container = document.getElementById('user-list');
+                users.forEach(user => {
+                    const div = document.createElement('div');
+                    div.className = 'list-group-item d-flex align-items-center';
+                    div.innerHTML = `
+                        <img src="${user.avatar}" style="width:70px; height:70px; border-radius:50%; margin-right:15px;">
+                        <span>${user.nickname}</span>
+                    `;
+                    container.appendChild(div);
+                });
+            });
+    </script>
+</div>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-
-        @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+</body>
 </html>
--- test2/src/routes/web.php
+++ test2/src/routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
+    return view('app');
+})->middleware('throttle:10,1');
